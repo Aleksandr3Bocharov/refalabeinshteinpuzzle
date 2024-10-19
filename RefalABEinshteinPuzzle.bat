f1339341fed8a2c245ea3a5be4a980d89b189a02
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-19 22:21:46 +0300

    Update RefalABEinshteinPuzzle.bat

--- RefalABEinshteinPuzzle.bat
+++ RefalABEinshteinPuzzle.bat
@@ -2,5 +2,5 @@ SET REFAL=F:\Programming\Projects\RefalAB
 
 %REFAL%\bin\RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
 as -o RefalABEinshteinPuzzle.o RefalABEinshteinPuzzle.asm >> RefalABEinshteinPuzzle.log
-gcc -o RefalABEinshteinPuzzle.exe %REFAL%\lib\mainrf.o RefalABEinshteinPuzzle.o -Wl,-L%REFAL%\lib -Wl,-lRefalAB >> RefalABEinshteinPuzzle.log
+clang -o RefalABEinshteinPuzzle.exe %REFAL%\lib\mainrf.o RefalABEinshteinPuzzle.o -Wl,-L%REFAL%\lib -Wl,-lRefalAB >> RefalABEinshteinPuzzle.log
 RefalABEinshteinPuzzle
