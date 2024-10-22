09db7c8409f0774eb90eb8e3c882f160a71a2010
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-22 16:13:07 +0300

    Add files via upload

--- RefalABEinshteinPuzzle.bat
+++ RefalABEinshteinPuzzle.bat
@@ -3,12 +3,10 @@
 rem Copyright 2024 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-10-20
+rem 2024-10-22
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
-SET REFAL=F:\Programming\Projects\RefalAB
-
-%REFAL%\bin\RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
+%REFALABBIN%\RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
 as -o RefalABEinshteinPuzzle.o RefalABEinshteinPuzzle.asm >> RefalABEinshteinPuzzle.log
-clang -o RefalABEinshteinPuzzle.exe %REFAL%\lib\mainrf.o RefalABEinshteinPuzzle.o -Wl,-L%REFAL%\lib -Wl,-lRefalAB >> RefalABEinshteinPuzzle.log
+clang -o RefalABEinshteinPuzzle.exe %REFALABLIB%\mainrf.o RefalABEinshteinPuzzle.o -Wl,-L%REFALABLIB% -Wl,-lRefalAB >> RefalABEinshteinPuzzle.log
 RefalABEinshteinPuzzle
