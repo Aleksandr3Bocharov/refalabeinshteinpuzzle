9d050b898f9d41d7ae3e05a5e6597b53710eedd4
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-14 10:56:40 +0200

    Update RefalABEinshteinPuzzle.bat

--- RefalABEinshteinPuzzle.bat
+++ RefalABEinshteinPuzzle.bat
@@ -1,9 +1,9 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-10-23
+rem 2025-03-14
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 %REFALABBIN%\RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
