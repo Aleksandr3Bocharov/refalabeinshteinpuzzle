decf0acce60b351aedc120d4c60ad42881e1581d
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-14 10:55:15 +0200

    Update RefalABEinshteinPuzzle.sh

--- RefalABEinshteinPuzzle.sh
+++ RefalABEinshteinPuzzle.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 
-# Copyright 2024 Aleksandr Bocharov
+# Copyright 2025 Aleksandr Bocharov
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-# 2024-10-23
+# 2024-03-14
 # https://github.com/Aleksandr3Bocharov/RefalAB
 
 $REFALABBIN/RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
