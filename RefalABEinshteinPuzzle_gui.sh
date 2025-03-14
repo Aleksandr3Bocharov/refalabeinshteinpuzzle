21144fcba5937c5eb08b474beb6bb017b03ec5f0
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-14 10:51:13 +0200

    Update RefalABEinshteinPuzzle_gui.sh

--- RefalABEinshteinPuzzle_gui.sh
+++ RefalABEinshteinPuzzle_gui.sh
@@ -3,7 +3,7 @@
 # Copyright 2025 Aleksandr Bocharov
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-# 2025-03-03
+# 2025-03-14
 # https://github.com/Aleksandr3Bocharov/RefalAB
 
 RAYLIBCFLAGS="-Wall -O2"
