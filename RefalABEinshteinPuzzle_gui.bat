0ea7f7e5114d5fa4bfe01cc220aae10b7e458392
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-14 10:51:34 +0200

    Update RefalABEinshteinPuzzle_gui.bat

--- RefalABEinshteinPuzzle_gui.bat
+++ RefalABEinshteinPuzzle_gui.bat
@@ -3,7 +3,7 @@
 rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2025-03-03
+rem 2025-03-14
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 set RAYLIB=F:\Programming\raylib-5.5_win64_mingw-w64
