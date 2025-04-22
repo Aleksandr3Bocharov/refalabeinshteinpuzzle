6301f1a9c718e1fc1850ad91a4f8038f3c10cb6c
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-04-22 18:58:00 +0300

    Add files via upload

--- RefalABEinshteinPuzzle.sh
+++ RefalABEinshteinPuzzle.sh
@@ -3,10 +3,12 @@
 # Copyright 2025 Aleksandr Bocharov
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-# 2024-03-14
+# 2024-04-21
 # https://github.com/Aleksandr3Bocharov/RefalAB
 
 $REFALABBIN/RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
 as -o RefalABEinshteinPuzzle.o RefalABEinshteinPuzzle.s >> RefalABEinshteinPuzzle.log
-clang -o RefalABEinshteinPuzzle RefalABEinshteinPuzzle.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB >> RefalABEinshteinPuzzle.log
+$REFALABBIN/RefalAB Solver >> RefalABEinshteinPuzzle.log
+as -o Solver.o Solver.s >> RefalABEinshteinPuzzle.log
+clang -o RefalABEinshteinPuzzle RefalABEinshteinPuzzle.o Solver.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB >> RefalABEinshteinPuzzle.log
 ./RefalABEinshteinPuzzle
--- RefalABEinshteinPuzzle_gui.sh
+++ RefalABEinshteinPuzzle_gui.sh
@@ -3,7 +3,7 @@
 # Copyright 2025 Aleksandr Bocharov
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-# 2025-03-14
+# 2025-04-21
 # https://github.com/Aleksandr3Bocharov/RefalAB
 
 RAYLIBCFLAGS="-Wall -O2"
@@ -11,7 +11,9 @@ RAYLIBLFLAGS="-lraylib -lm"
 
 $REFALABBIN/RefalAB RefalABEinshteinPuzzle_gui > RefalABEinshteinPuzzle_gui.log
 as -o RefalABEinshteinPuzzle_gui.o RefalABEinshteinPuzzle_gui.s >> RefalABEinshteinPuzzle_gui.log
+$REFALABBIN/RefalAB Solver >> RefalABEinshteinPuzzle_gui.log
+as -o Solver.o Solver.s >> RefalABEinshteinPuzzle_gui.log
 clang $REFALABCFLAGS -I$REFALABINCLUDE -c RefalABEinshteinPuzzlec_gui.c -o RefalABEinshteinPuzzlec_gui.o >> RefalABEinshteinPuzzle_gui.log
 clang $RAYLIBCFLAGS -c gui.c -o gui.o >> RefalABEinshteinPuzzle_gui.log
-clang -o RefalABEinshteinPuzzle_gui RefalABEinshteinPuzzle_gui.o RefalABEinshteinPuzzlec_gui.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB gui.o $RAYLIBLFLAGS >> RefalABEinshteinPuzzle_gui.log
+clang -o RefalABEinshteinPuzzle_gui RefalABEinshteinPuzzle_gui.o RefalABEinshteinPuzzlec_gui.o Solver.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB gui.o $RAYLIBLFLAGS >> RefalABEinshteinPuzzle_gui.log
 ./RefalABEinshteinPuzzle_gui
