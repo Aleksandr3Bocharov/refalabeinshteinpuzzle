48293e21c4b2bcb7aa44db978b2adc7912be0678
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-07-28 15:56:16 +0300

    Add files via upload

--- make_run.bat
+++ make_run.bat
@@ -3,12 +3,13 @@
 rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2025-03-21
+rem 2025-07-28
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
-%REFALABBIN%\RefalAB RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
-as -o RefalABEinshteinPuzzle.o RefalABEinshteinPuzzle.s >> RefalABEinshteinPuzzle.log
-%REFALABBIN%\RefalAB Solver >> RefalABEinshteinPuzzle.log
-as -o Solver.o Solver.s >> RefalABEinshteinPuzzle.log
-clang -o RefalABEinshteinPuzzle.exe RefalABEinshteinPuzzle.o Solver.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB >> RefalABEinshteinPuzzle.log
-RefalABEinshteinPuzzle
+if not exist bin mkdir bin
+%REFALABBIN%\RefalAB src\RefalABEinshteinPuzzle > RefalABEinshteinPuzzle.log
+as -o src\RefalABEinshteinPuzzle.o src\RefalABEinshteinPuzzle.s >> RefalABEinshteinPuzzle.log
+%REFALABBIN%\RefalAB src\Solver >> RefalABEinshteinPuzzle.log
+as -o src\Solver.o src\Solver.s >> RefalABEinshteinPuzzle.log
+clang -o bin\RefalABEinshteinPuzzle.exe src\RefalABEinshteinPuzzle.o src\Solver.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB >> RefalABEinshteinPuzzle.log
+bin\RefalABEinshteinPuzzle
--- make_run_gui.bat
+++ make_run_gui.bat
@@ -3,18 +3,19 @@
 rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2025-03-21
+rem 2025-07-28
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 set RAYLIB=F:\Programming\raylib-5.5_win64_mingw-w64
 set RAYLIBCFLAGS=-Wall -O2 -I%RAYLIB%\include
 set RAYLIBLFLAGS=-L%RAYLIB%\lib -lraylib -lgdi32 -lwinmm
 
-%REFALABBIN%\RefalAB RefalABEinshteinPuzzle_gui > RefalABEinshteinPuzzle_gui.log
-as -o RefalABEinshteinPuzzle_gui.o RefalABEinshteinPuzzle_gui.s >> RefalABEinshteinPuzzle_gui.log
-%REFALABBIN%\RefalAB Solver >> RefalABEinshteinPuzzle_gui.log
-as -o Solver.o Solver.s >> RefalABEinshteinPuzzle_gui.log
-clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c RefalABEinshteinPuzzlec_gui.c -o RefalABEinshteinPuzzlec_gui.o >> RefalABEinshteinPuzzle_gui.log
-clang %RAYLIBCFLAGS% -c gui.c -o gui.o >> RefalABEinshteinPuzzle_gui.log
-clang -o RefalABEinshteinPuzzle_gui.exe RefalABEinshteinPuzzle_gui.o RefalABEinshteinPuzzlec_gui.o Solver.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB gui.o %RAYLIBLFLAGS% >> RefalABEinshteinPuzzle_gui.log
-RefalABEinshteinPuzzle_gui
+if not exist bin mkdir bin
+%REFALABBIN%\RefalAB src\RefalABEinshteinPuzzle_gui > RefalABEinshteinPuzzle_gui.log
+as -o src\RefalABEinshteinPuzzle_gui.o src\RefalABEinshteinPuzzle_gui.s >> RefalABEinshteinPuzzle_gui.log
+%REFALABBIN%\RefalAB src\Solver >> RefalABEinshteinPuzzle_gui.log
+as -o src\Solver.o src\Solver.s >> RefalABEinshteinPuzzle_gui.log
+clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c src\RefalABEinshteinPuzzlec_gui.c -o src\RefalABEinshteinPuzzlec_gui.o >> RefalABEinshteinPuzzle_gui.log
+clang %RAYLIBCFLAGS% -c src\gui.c -o src\gui.o >> RefalABEinshteinPuzzle_gui.log
+clang -o bin\RefalABEinshteinPuzzle_gui.exe src\RefalABEinshteinPuzzle_gui.o src\RefalABEinshteinPuzzlec_gui.o src\Solver.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB src\gui.o %RAYLIBLFLAGS% >> RefalABEinshteinPuzzle_gui.log
+bin\RefalABEinshteinPuzzle_gui
