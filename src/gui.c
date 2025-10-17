40936aa72572ff478d50a86da672c03a6b47926f
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-17 21:52:26 +0300

    Add files via upload

--- src/gui.c
+++ src/gui.c
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 // 2025-04-07
-// https://github.com/Aleksandr3Bocharov/refalabeinshteinpuzzle
+// https://github.com/Aleksandr3Bocharov/refalabeinsteinpuzzle
 
 //====================================================================
 // Модуль "gui" (GUI raygui raylib версия)
--- src/refalabeinshteinpuzzlec_gui.c
+++ src/refalabeinshteinpuzzlec_gui.c
@@ -2,10 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 // 2025-10-07
-// https://github.com/Aleksandr3Bocharov/refalabeinshteinpuzzle
+// https://github.com/Aleksandr3Bocharov/refalabeinsteinpuzzle
 
 //====================================================================
-// Модуль "refalabeinshteinpuzzlec" (GUI версия)
+// Модуль "refalabeinsteinpuzzlec" (GUI версия)
 //====================================================================
 
 #include <stdint.h>
