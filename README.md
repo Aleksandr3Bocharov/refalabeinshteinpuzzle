20dd413ff403c9da3c574f7f7052bd97288a907e
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-10-17 21:51:59 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-RefalABEinshteinPuzzle
+RefalABEinsteinPuzzle
 ------------------------
 
-Программа "RefalABEinshteinPuzzle" написана на языке программирования [РефалАБ](https://github.com/Aleksandr3Bocharov/refalab).
+Программа "RefalABEinsteinPuzzle" написана на языке программирования [РефалАБ](https://github.com/Aleksandr3Bocharov/refalab).
 
 Список командных файлов для сборки программы:
 - make_run.bat для компиляции, компоновки и запуска программы в Windows;
--- make_run.bat
+++ make_run.bat
@@ -4,10 +4,10 @@ rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 rem 2025-10-17
-rem https://github.com/Aleksandr3Bocharov/refalabeinshteinpuzzle
+rem https://github.com/Aleksandr3Bocharov/refalabeinsteinpuzzle
 
 if not exist bin mkdir bin
-%REFALABBIN%\refalabc src\refalabeinshteinpuzzle -a,"-o src\refalabeinshteinpuzzle.o" > refalabeinshteinpuzzle.log
-%REFALABBIN%\refalabc src\solver -a,"-o src\solver.o" >> refalabeinshteinpuzzle.log
-clang -o bin\refalabeinshteinpuzzle.exe src\refalabeinshteinpuzzle.o src\solver.o %REFALABLIB%\obj\mainrf.o -L%REFALABLIB% -lrefalab >> refalabeinshteinpuzzle.log
-bin\refalabeinshteinpuzzle
+%REFALABBIN%\refalabc src\refalabeinsteinpuzzle -a,"-o src\refalabeinsteinpuzzle.o" > refalabeinsteinpuzzle.log
+%REFALABBIN%\refalabc src\solver -a,"-o src\solver.o" >> refalabeinsteinpuzzle.log
+clang -o bin\refalabeinsteinpuzzle.exe src\refalabeinsteinpuzzle.o src\solver.o %REFALABLIB%\obj\mainrf.o -L%REFALABLIB% -lrefalab >> refalabeinsteinpuzzle.log
+bin\refalabeinsteinpuzzle
